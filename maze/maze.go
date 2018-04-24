e4fe7a7de0aefefaf42336e4ee512840db30a191
RaistlinD <Johnd0712@gmail.com>
2018-04-24 22:09:16 +0800

    ok

--- maze/maze.go
+++ maze/maze.go
@@ -13,7 +13,7 @@ func readMaze(filename string) [][]int {
 
 	var row, col int
 	fmt.Fscanf(file, "%d %d", &row, &col)
-
+	fmt.Println("row is ",row, " col is " , col)
 	maze := make([][]int,row)
 	for i := range maze {
 		maze[i] = make ([]int , col)
@@ -21,8 +21,15 @@ func readMaze(filename string) [][]int {
 			fmt.Fscanf(file, "%d", &maze[i][j])
 		}
 	}
+	return maze
 }
 
 func main(){
-	readMaze("maze/maze.in")
+	maze :=	readMaze("maze/maze.in")
+	for i := range maze	{
+		for j := range maze[i]{
+			fmt.Print(maze[i][j]," ")
+		}
+		fmt.Println()
+	}
 }
