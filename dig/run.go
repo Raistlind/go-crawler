eda09d5ce8ca3c4a5e08fe5a9e55fe35e0f2beac
RaistlinD <Johnd0712@gmail.com>
2018-05-18 23:52:38 +0800

    ok

--- dig/run.go
+++ dig/run.go
@@ -5,7 +5,24 @@ import (
 	"fmt"
 	"strings"
 	"strconv"
+	"net/url"
+	"math/rand"
+	"time"
+	"os"
 )
+
+var uaList = []string{
+	"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36 OPR/26.0.1656.60",
+	"Opera/8.0 (Windows NT 5.1; U; en)",
+	"Mozilla/5.0 (Windows NT 5.1; U; en; rv:1.8.1) Gecko/20061208 Firefox/2.0.0 Opera 9.50",
+	"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; en) Opera 9.50",
+	"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0",
+	"Mozilla/5.0 (X11; U; Linux x86_64; zh-CN; rv:1.9.2.10) Gecko/20100922 Ubuntu/10.10 (maverick) Firefox/3.6.10",
+	"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.57.2 (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2",
+	"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36",
+	"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11",
+	"Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.16 (KHTML, like Gecko) Chrome/10.0.648.133 Safari/534.16",
+}
 
 type resource struct {
 	url    string
@@ -57,6 +74,28 @@ func buildUrl(res []resource) []string {
 	return list
 }
 
+func makeLog(current, refer, ua string) string {
+	u := url.Values{}
+	u.Set("time", 1)
+	u.Set("url", current)
+	u.Set("refer", refer)
+	u.Set("ua", ua)
+	paramsStr := u.Encode()
+
+	logTemplate := `127.0.0.1 - - [17/May/2018:22:21:19 +0800] "OPTIONS /dig?{$paramsStr} HTTP/1.1" 200 43 "http://localhost:7888/" "{$ua}" "-"`
+	log := strings.Replace(logTemplate, "{$paramsStr}", paramsStr, -1)
+	log = strings.Replace(log, "{$ua}", ua, -1)
+	return log
+}
+
+func randInt(min, max int) int {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	if min > max {
+		return max
+	}
+	return r.Intn(max-min) + min
+}
+
 func main() {
 	total := flag.Int("total", 100, "how many")
 	filePath := flag.String("filePath", "/usr/local/nginx/logs/dig.log", "file path")
@@ -65,6 +104,20 @@ func main() {
 	res := ruleResource()
 	list := buildUrl(res)
 
+	logStr := ""
+	for i := 0; i <= *total; i ++ {
+		currentUrl := list[randInt(0, len(list)-1)]
+		referUrl := list[randInt(0, len(list)-1)]
+		ua := uaList[randInt(0, len(uaList)-1)]
+
+		logStr = logStr + makeLog(currentUrl, referUrl, ua) + "\n"
+		//ioutil.WriteFile(*filePath, []byte(logStr), 0644)
+	}
+
+	fd, _ := os.OpenFile(*filePath, os.O_RDWR|os.O_APPEND, 0644)
+	fd.Write([]byte(logStr))
+
+	defer fd.Close()
 	fmt.Println(total, filePath, list)
 	fmt.Println("done. ")
 }
